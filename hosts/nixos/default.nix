d812548ec5595f2b2dddfa9a4d2aa5202ed83ca3
sunick2009 <sunick2009@gmail.com>
2025-08-23 18:41:48 +0800

    更新時區設定，將時區從 America/New_York 調整為 Asia/Taipei

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -27,7 +27,7 @@ let user = "susu";
   };
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Taipei";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
