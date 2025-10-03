42ccf0f6399ec6cd0db75b7ed9bdd5492cc14245
sunick2009 <sunick2009@gmail.com>
2025-10-03 15:31:28 +0800

    feat(darwin): 添加 yazi 當作 TUI File Manager 及其相關工具

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -9,4 +9,20 @@ shared-packages ++ [
   iina
   # Disk usage analyzer
   grandperspective
+  # TUI File manager
+  yazi
+
+    # yazi related tools
+    fd
+    ripgrep
+    fzf
+    bat
+    jq
+    p7zip
+    exiftool
+    mediainfo
+    poppler          # pdftotext/pdftoppm 等，PDF 預覽
+    imagemagick
+    ffmpegthumbnailer
+    zoxide           # 跟 yazi 很搭的目錄跳轉
 ]
